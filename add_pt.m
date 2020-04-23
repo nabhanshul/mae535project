a0014f34c9ce06075b733c2aeed8e8f7794e6720
nabhanshul <59465940+nabhanshul@users.noreply.github.com>
2020-04-23 02:26:03 -0400

    Update add_pt.m

--- add_pt.m
+++ add_pt.m
@@ -4,12 +4,5 @@ function [point] =add_pt(x,y)
     for i=1:(length(x)-1)
         mi_addsegment(x(i),y(i),x(i+1),y(i+1));
     end
-%     mi_addsegment(85,36,85,0);
-    
-%       for i=1:length(x)
-%         mi_selectsegment(x(i),y(i));
-%         %mi_mirror(85,1,85,2)
-%        % mi_mirror(85,0,85,35)
-%       end  
     
 end
